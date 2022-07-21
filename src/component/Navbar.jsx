5396fb539ce25a2e164f1ef2cc105bd5adb45860
THEO MICALETTI <theo.micaletti@icloud.com>
2022-07-21 16:45:19 +0200

    wip

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <>
       <nav>
-        <ul className="drop-shadow-[-4px_-4px_0px_rgba(160,160,160)] bg-gradient-to-r from-purple via-pink to-yellow p-2 pb-4 border-4 border-silver flex justify-around text-2xl w-[90%] mx-auto font-[400] mt-6 mb-20">
+        <ul className="drop-shadow-[-4px_-4px_0px_rgba(160,160,160)] bg-gradient-to-r from-purple via-pink to-yellow p-3 pb-4 border-4 border-silver flex justify-around text-2xl w-[90%] mx-auto font-[400] mt-6 mb-20">
           <a href="https://github.com/TheoMicaletti" target="_blank">
             <li className="cursor-pointer px-12 mt-1.5 hover:text-[#ffffff] md:inline">
               GitHub
--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -1,6 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+let email = [
+  { id: 1, letter: "t" },
+  { id: 2, letter: "h" },
+  { id: 3, letter: "e" },
+  { id: 4, letter: "o" },
+  { id: 5, letter: "." },
+  { id: 6, letter: "m" },
+  { id: 7, letter: "i" },
+  { id: 8, letter: "c" },
+  { id: 9, letter: "a" },
+  { id: 10, letter: "l" },
+  { id: 11, letter: "e" },
+  { id: 12, letter: "t" },
+  { id: 13, letter: "t" },
+  { id: 14, letter: "i" },
+  { id: 15, letter: "@" },
+  { id: 16, letter: "i" },
+  { id: 17, letter: "c" },
+  { id: 18, letter: "l" },
+  { id: 19, letter: "o" },
+  { id: 20, letter: "u" },
+  { id: 21, letter: "d" },
+  { id: 22, letter: "." },
+  { id: 23, letter: "c" },
+  { id: 24, letter: "o" },
+  { id: 25, letter: "m" },
+];
+
 function Contact() {
   return (
     <>
@@ -10,8 +38,17 @@ function Contact() {
         </div>
       </Link>
       <div className="text-center mt-40 text-2xl">
-        <p>You can send an email to :</p>
-        <h3 className="mt-4 font-[600] text-4xl">theo.micaletti@icloud.com</h3>
+        <p className="mb-4">Click on the email send a message to me !</p>
+        <a
+          className="flex justify-center font-[600] text-5xl hover:text-yellow"
+          href="mailto:theo.micaletti@icloud.com"
+        >
+          {email.map((first) => (
+            <div className="flex-row hover:font-[800] hover:animate-waving-hand hover:text-pink">
+              {first.letter}
+            </div>
+          ))}
+        </a>
       </div>
     </>
   );
